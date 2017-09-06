1fd7db95fa0e71ef6383c6b7e42c07176981c7df
Pavel <imortan@gmail.com>
2017-09-06 03:08:57 +0300

    FileParamsDeviceConnector created

--- client/src/main/java/com/github/e13mort/stf/console/commands/connect/ConnectCommand.java
+++ client/src/main/java/com/github/e13mort/stf/console/commands/connect/ConnectCommand.java
@@ -3,6 +3,7 @@ package com.github.e13mort.stf.console.commands.connect;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.beust.jcommander.ParametersDelegate;
+import com.beust.jcommander.converters.FileConverter;
 import com.beust.jcommander.converters.IntegerConverter;
 import com.github.e13mort.stf.client.FarmClient;
 import com.github.e13mort.stf.console.AdbRunner;
@@ -11,6 +12,7 @@ import com.github.e13mort.stf.console.commands.ConsoleDeviceParamsImpl;
 import com.github.e13mort.stf.console.commands.cache.DeviceListCache;
 import io.reactivex.Completable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -28,6 +30,8 @@ public class ConnectCommand implements CommandContainer.Command {
     @Parameter(names = "-l", variableArity = true, listConverter = IntegerConverter.class,
             description = "Connect to devices by its indexes from the results of previous \"devices\" command. E.g. \"-l 1 2 5\"")
     private List<Integer> devicesIndexesFromCache = new ArrayList<>();
+    @Parameter(names = "-f", description = "Read connection params from a file", converter = FileConverter.class)
+    private File storedConnectionParamsFile;
     private DeviceListCache cache;
 
     public ConnectCommand(FarmClient client, AdbRunner adbRunner, DeviceListCache cache, Logger logger) {
@@ -53,6 +57,8 @@ public class ConnectCommand implements CommandContainer.Command {
             connector = new MyDevicesConnector(client, adbRunner, logger);
         } else if (!devicesIndexesFromCache.isEmpty()) {
             connector = new CacheDevicesConnector(client, adbRunner, logger, devicesIndexesFromCache, cache);
+        } else if (storedConnectionParamsFile != null) {
+            connector = new FileParamsDeviceConnector(client, adbRunner, logger, storedConnectionParamsFile);
         } else {
             connector = new ParamsConnector(client, adbRunner, logger, params);
         }
---
+++ client/src/test/java/com/github/e13mort/stf/console/commands/connect/FileParamsDeviceConnectorTest.java
@@ -0,0 +1,85 @@
+package com.github.e13mort.stf.console.commands.connect;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.github.e13mort.stf.client.FarmClient;
+import com.github.e13mort.stf.console.AdbRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+class FileParamsDeviceConnectorTest {
+    @Mock
+    private FarmClient farmClient;
+    @Mock
+    private AdbRunner adbRunner;
+    @Mock
+    private Logger logger;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @DisplayName("valid params files parsing")
+    @ParameterizedTest(name = "file: {0}")
+    @ValueSource(strings = {
+            "valid_min_api.json",
+            "valid_max_api.json",
+            "valid_api.json",
+            "valid_count.json",
+            "valid_abi.json",
+            "valid_names.json",
+            "valid_providers.json",
+            "valid_serials.json"
+    })
+    void readParamsFromFile_validFiles_success(String testFileName) throws IOException {
+        final FileParamsDeviceConnector connector = createConnector(testFileName);
+        connector.readParamsFromFile();
+    }
+
+    @DisplayName("invalid params should throw exception")
+    @ParameterizedTest(name = "param file: {0} -> exception: {1}")
+    @MethodSource("invalidFiles")
+    void readParamsFromFile_invalidFiles_jsonException(String fileName, Class<Throwable> exceptionClass) {
+        final FileParamsDeviceConnector connector = createConnector(fileName);
+        Assertions.assertThrows(exceptionClass, connector::readParamsFromFile);
+    }
+
+    @Test
+    void readParamsFromFile_validFullFile_exactMatching() {
+        //implement
+    }
+
+    private static Stream<Arguments> invalidFiles() {
+        return Stream.of(
+                Arguments.of("invalid_field_name.json", JsonMappingException.class),
+                Arguments.of("invalid_field_value_simple.json", JsonMappingException.class),
+                Arguments.of("invalid_field_value_custom.json", JsonMappingException.class),
+                Arguments.of("not_a_file", IOException.class)
+        );
+    }
+
+    private FileParamsDeviceConnector createConnector(String testFileName) {
+        final File paramsFile = getFile(testFileName);
+        return new FileParamsDeviceConnector(farmClient, adbRunner, logger, paramsFile);
+    }
+
+    private File getFile(String testFileName) {
+        final URL resource = getClass().getClassLoader().getResource("connection_params/" + testFileName);
+        return new File(resource != null ? resource.getFile() : "not_existing_file");
+    }
+
+}
